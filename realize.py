2843428fe796409bb8ee3e51d3e613697c75d4a1
Tristan Trouwen <tristan.trouwen@gmail.com>
2023-09-11 15:01:07 +0200

    Add realization to score.

--- realize.py
+++ realize.py
@@ -14,7 +14,14 @@ if __name__ == '__main__':
     )
     logging.log(logging.INFO, 'Started realizing.')
     file_path = Path.cwd() / "test_file.musicxml"
-    part = converter.parse(file_path).parts[-1]
-    fbLine2 = realizer.figuredBassFromStream(part)
+    parts = converter.parse(file_path).parts
+    basso_continuo = parts[-1]
+
+    fbLine2 = realizer.figuredBassFromStream(basso_continuo)
     fbRealization2 = fbLine2.realize()
-    fbRealization2.generateRandomRealization().show()
+    realized = fbRealization2.generateRandomRealization()
+
+    stream = parts.stream()
+    for part in realized.parts:
+        stream.append(part)
+    stream.show()
