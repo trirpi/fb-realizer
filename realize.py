40d67813e827f3517afc51d86020e1d8f4378e84
Tristan Trouwen <tristan.trouwen@gmail.com>
2023-09-12 09:47:29 +0200

    Added more info and use latest version of music21 fork.

--- realize.py
+++ realize.py
@@ -17,6 +17,8 @@ if __name__ == '__main__':
     parts = converter.parse(file_path).parts
     basso_continuo = parts[-1]
 
+    basso_continuo.beatAndMeasureFromOffset()
+    basso_continuo.flatten().getElementsByOffset(4).notes[0]
     fbLine2 = realizer.figuredBassFromStream(basso_continuo)
     fbRealization2 = fbLine2.realize()
     realized = fbRealization2.generateRandomRealization()
