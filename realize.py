24aa1952ff5499937e516f494489ac56cb362372
Tristan Trouwen <tristan.trouwen@gmail.com>
2023-10-03 17:36:53 +0200

    Start on splitting on rest.

--- realize.py
+++ realize.py
@@ -7,6 +7,75 @@ from music21.dynamics import Dynamic
 from music21.improvedFiguredBass import realizer
 from music21.note import GeneralNote
 
+
+def add_melody_notes(segments, melody_parts):
+    """Fills in the segments melody_notes attribute."""
+    idxs = [0] * len(melody_parts)
+    for segment in segments:
+        start_offset = segment.play_offsets[0]
+        for i, part in enumerate(melody_parts):
+            elts = part.notesAndRests
+            while idxs[i] < len(elts) and elts[idxs[i]].offset < start_offset:
+                idxs[i] += 1
+            if not (idxs[i] < len(elts) and elts[idxs[i]].offset == start_offset):
+                if idxs[i] == 0: continue
+                idxs[i] -= 1
+            melody_note: GeneralNote = elts[idxs[i]]
+            if melody_note.isNote:
+                segment.melody_notes.add(elts[idxs[i]])
+
+
+def add_dynamic_markings(segments, melody_parts):
+    """Fills in the segments dynamic attribute."""
+    parts = [part.getElementsByClass(Dynamic) for part in melody_parts]
+    idxs = [0] * len(melody_parts)
+    for segment in segments:
+        start_offset = segment.play_offsets[0]
+        for i, elts in enumerate(parts):
+            while idxs[i] < len(elts) and elts[idxs[i]].offset < start_offset:
+                idxs[i] += 1
+            if not (idxs[i] < len(elts) and elts[idxs[i]].offset == start_offset):
+                if idxs[i] == 0: continue
+                idxs[i] -= 1
+            segment.dynamic = elts[idxs[i]].value
+
+
+def split_on_rests(bc, melodies):
+    result = []
+
+    current_melodies = melodies
+    current = bc
+    rests = []
+    while current:
+        rest = None
+        for note in list(current):
+            if note.isRest:
+                rest = note
+                break
+        if rest:
+            rests.append(rest)
+            rest_start = rest.offset
+            rest_length = rest.quarterLength
+            rest_end = rest_start + rest_length
+            prev, current = current.splitAtQuarterLength(rest_start)
+            _, current = current.splitAtQuarterLength(rest_length)
+            current = current.notesAndRests
+            prev_mels = []
+            new_mels = []
+            for mel in current_melodies:
+                prev_mel, curr_mel = mel.splitAtQuarterLength(rest_end)
+                prev_mels.append(prev_mel)
+                new_mels.append(curr_mel)
+            current_melodies = new_mels
+
+            result.append((prev, prev_mels))
+        else:
+            result.append((current, current_melodies))
+            current = None
+
+    return result, rests
+
+
 if __name__ == '__main__':
     logging.basicConfig(
         stream=sys.stdout,
@@ -15,50 +84,27 @@ if __name__ == '__main__':
         datefmt='%H:%M:%S',
     )
     logging.log(logging.INFO, 'Started realizing.')
-    file_path = Path.cwd() / "test_pieces/Erhore_mich_wenn_ich_rufe_Schutz4.musicxml"
-    # file_path = Path.cwd() / "test_pieces/Oboe_Concerto_in_D_minor_Op9_No2__Tomaso_Albinoni.musicxml"
+    # file_path = Path.cwd() / "test_pieces/Erhore_mich_wenn_ich_rufe_Schutz4.musicxml"
+    file_path = Path.cwd() / "test_pieces/Oboe_Concerto_in_D_minor_Op9_No2__Tomaso_Albinoni.musicxml"
     # file_path = Path.cwd() / "test_pieces/test_file.musicxml"
     parts = converter.parse(file_path).parts
-    basso_continuo = parts[-1]
+    basso_continuo = parts[-1].flatten().notesAndRests
 
     # split fbLine on rests
-    # split melody pieces on rests as well?
+    melody_parts = [p.flatten() for p in parts[:-1]]
+    tups, rests = split_on_rests(basso_continuo, melody_parts)
 
     logging.log(logging.INFO, 'Parse stream to figured bass.')
     fbLine = realizer.figuredBassFromStream(basso_continuo)
     fbRealization = fbLine.realize()
 
     logging.log(logging.INFO, 'Parse melody notes.')
-    melody_parts = [p.flatten() for p in parts[:-1]]
-    current_idx = [0 for _ in melody_parts]
-    for segment in fbRealization._segmentList:
-        start_offset = segment.play_offsets[0]
-        for i, part in enumerate(melody_parts):
-            elts = part.notesAndRests
-            while current_idx[i] < len(elts) and elts[current_idx[i]].offset < start_offset:
-                current_idx[i] += 1
-            if not (current_idx[i] < len(elts) and elts[current_idx[i]].offset == start_offset):
-                if current_idx[i] == 0: continue
-                current_idx[i] -= 1
-            melody_note: GeneralNote = elts[current_idx[i]]
-            if melody_note.isNote:
-                segment.melody_notes.add(elts[current_idx[i]])
+    add_melody_notes(fbRealization._segmentList, melody_parts)
 
     logging.log(logging.INFO, 'Parse dynamic markings.')
-    dynamic_idx = 0
-    dynamic_elts = melody_parts[0].getElementsByClass(Dynamic)
-    for segment in fbRealization._segmentList:
-        segment.dynamic = 'mf'
-        start_offset = segment.play_offsets[0]
-        while dynamic_idx < len(dynamic_elts) and dynamic_elts[dynamic_idx].offset < start_offset:
-            dynamic_idx += 1
-        if not (dynamic_idx < len(dynamic_elts) and dynamic_elts[dynamic_idx].offset == start_offset):
-            if dynamic_idx == 0: continue
-            dynamic_idx -= 1
-        segment.dynamic = dynamic_elts[dynamic_idx].value
+    add_dynamic_markings(fbRealization._segmentList, melody_parts)
 
     logging.log(logging.INFO, 'Generating optimal realization.')
-    # realized = fbRealization2.generateRandomRealization()
     realized = fbRealization.generate_optimal_realization()
 
     stream = parts.stream()
