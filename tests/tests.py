5e6cac296a5e6f2dac3efe6b1d63647a1b756717
Tristan Trouwen <16321928+trirpi@users.noreply.github.com>
2023-11-28 17:30:35 +0100

    Created function to get intermediate notes.

--- tests/tests.py
+++ tests/tests.py
@@ -2,27 +2,57 @@ from pathlib import Path
 
 import pytest
 
+from music21.pitch import Pitch
 from realize import realize_from_path
 from config import pieces, default_piece
 from music21.improvedFiguredBass.segment import Segment, SegmentOption
+from music21.improvedFiguredBass.possibility import Possibility
 
 
-def test_segment():
-    s = Segment('D', '6,4')
+@pytest.fixture
+def segment() -> Segment:
+    s = Segment('D3', '6,4')
     s.set_pitch_names_in_chord()
     s.finish_initialization()
-    assert hasattr(s, 'dynamic')
-    assert hasattr(s, 'next_segment')
-    assert hasattr(s, 'prev_segment')
-    assert len(s.segment_options) == 1
+    return s
 
 
-def test_segment_options():
-    s = Segment('D', '6,4')
-    s.set_pitch_names_in_chord()
-    s.finish_initialization()
-    segment_option: SegmentOption = s.segment_options[0]
+@pytest.fixture
+def segment_option(segment) -> SegmentOption:
+    return segment.segment_options[0]
+
+
+@pytest.fixture
+def possibility() -> Possibility:
+    return Possibility((Pitch('D3'), Pitch('G3'), Pitch('B3')), 0)
+
+
+def test_segment(segment):
+    assert hasattr(segment, 'dynamic')
+    assert hasattr(segment, 'next_segment')
+    assert hasattr(segment, 'prev_segment')
+    assert len(segment.segment_options) == 1
+
+
+def test_intermediate_note(segment, possibility):
+    intermediate_notes = segment.get_intermediate_notes(possibility)
+    assert len(intermediate_notes) == 6
+    pitch, voice = intermediate_notes[0]
+    assert voice == 0
+    assert pitch == Pitch('E3')
+    pitch, voice = intermediate_notes[1]
+    assert voice == 0
+    assert pitch == Pitch('C3')
+    pitch, voice = intermediate_notes[5]
+    assert voice == 2
+    assert pitch == Pitch('A3')
+
+
+def test_segment_options(segment_option):
     assert tuple(segment_option.pitch_names_in_chord) == ('D', 'G', 'B')
+
+
+
 
 
 def test_realization():
